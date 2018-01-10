9fba8cda59e04af5eecde9fecfcf73a524a98553
Elijah Hill <elijahhill@u.boisestate.edu>
2018-01-10 04:59:43 -0700

    Got the scraping part working, now just to parse through it and figure out the correct times.

---
+++ scrape.py
@@ -0,0 +1,32 @@
+import selenium
+from selenium import webdriver
+
+import bs4
+from bs4 import BeautifulSoup
+
+import time
+from datetime import datetime
+
+import urllib
+from urllib import request
+
+# address = "http://shuttle.boisestate.edu/"
+#
+# driver = webdriver.Chrome()
+#
+# driver.get(address)
+#
+# f = urllib.request.urlopen(address)
+# time.sleep(5.0)
+#
+# soup = BeautifulSoup(f, "html.parser")
+#
+# div = soup.find(id="routes")
+#
+# print(div)
+
+print(datetime.time(datetime.now()))
+
+datetime.time(datetime.now(min()))
+
+# driver.close()
